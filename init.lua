a5c2dca58d4199edbef59b90228d84020dcb0375
Honza Rychly <honza@axiomdatascience.com>
2026-04-03 15:50:02 -0800

    Add custom gruvbox based colorscheme

--- init.lua
+++ init.lua
@@ -759,11 +759,8 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
@@ -774,12 +771,20 @@ require('lazy').setup({
         },
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Activate the colorscheme:
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
+
+  -- { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true, opts = ... },
+  {
+    'honzaflash/gruvyboxy.nvim',
+    priority = 1000,
+    opts = ...,
+    config = function() vim.cmd.colorscheme 'gruvyboxy' end,
+  },
+  -- for theme develpment purposes load the local version
+  -- { dir = '~/dev/nvim/gruvyboxy.nvim' },
 
   -- Highlight todo, notes, etc in comments
   {
