893ccb8a6c1a1c5e6bb0710b2017b55d69cae5f0
Honza Rychly <honza@axiomdatascience.com>
2026-04-22 09:17:55 -0800

    Add keymap for last tab/buffer

--- init.lua
+++ init.lua
@@ -158,6 +158,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Quick switch between files
+vim.keymap.set('n', 'gl', '<C-^>', { desc = "Switch to the [L]ast buffer" })
+vim.keymap.set('n', 'ga', 'g<Tab>', { desc = "Switch to the l[A]st tab" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
